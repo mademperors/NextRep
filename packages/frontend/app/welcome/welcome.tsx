b8b4bc55991daca2deaa1f6371a6b3929a32125c
Myroslav <verstyukm@gmail.com>
2025-06-08 14:29:02 +0300

    Prettier home page

--- packages/frontend/app/welcome/welcome.tsx
+++ packages/frontend/app/welcome/welcome.tsx
@@ -1,7 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 import { checkAuth } from '~/api/auth';
-import logoDark from './logo-dark.svg';
-import logoLight from './logo-light.svg';
 
 export function Welcome() {
   const { data, isLoading } = useQuery({
@@ -10,21 +8,19 @@ export function Welcome() {
   });
 
   if (isLoading) return <div>Loading...</div>;
-  if (data) return <div>Logged in {data.email}</div>;
 
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img src={logoLight} alt="React Router" className="block w-full dark:hidden" />
-            <img src={logoDark} alt="React Router" className="hidden w-full dark:block" />
-          </div>
+          <h1 className="text-8xl font-bold font-fugaz">NextRep</h1>
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              Welcome to NextRep! Please login to continue.
+              {data
+                ? `Welcome back, ${data.email}!`
+                : 'Welcome to NextRep! Please login to continue.'}
             </p>
           </nav>
         </div>
