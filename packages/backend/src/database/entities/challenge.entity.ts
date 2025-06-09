95679d59a8115e5416a60b94d8896bebce14bc2e
MaksymSemk <maksumsemen2006@gmail.com>
2025-06-09 14:17:59 +0300

    changed string to enum in challenge-type

--- packages/backend/src/database/entities/challenge.entity.ts
+++ packages/backend/src/database/entities/challenge.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Member } from './member.entity';
+import { ChallengeType } from 'src/constants/enums/challenge-types.enum';
 
 @Entity('challenge')
 export class Challenge {
@@ -13,7 +14,7 @@ export class Challenge {
   challenge_info: string;
 
   @Column({ type: 'varchar', length: 50, nullable: false })
-  challenge_type: string;
+  challenge_type: ChallengeType;
 
   @ManyToOne(() => Member, (member) => member.email, { onDelete: 'CASCADE' })
   @JoinColumn({ referencedColumnName: 'email' })
